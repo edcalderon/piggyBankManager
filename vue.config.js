94c35edc27fbab8ca00d3f3069872db84880e25c
ed <edwardca12@gmail.com>
2020-08-28 23:47:42 -0400

    server

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/eth-wallet/'
+    ? '/'
     : '/',
-  outputDir: 'docs',
+  outputDir: 'dist',
   configureWebpack: {
     plugins: [new Dotenv()]
   }
